649fc107e29fb693682282f1578fad1f148c0c64
ssingh <ssingh@artefactual.com>
2024-12-06 14:33:31 -0800

    day6 part1

---
+++ 2024/day6/part1.py
@@ -0,0 +1,73 @@
+# row, col and x,y are backwards
+# so I'm just gonna just stick with row, col
+
+file = "./input1.txt"
+
+# 10, 130
+padding_width = 130
+
+pad = ["$" for _ in range(10 + 2)]
+
+board = [pad]
+
+start_found = False
+
+# one indexed since we pad
+start_row = 1
+start_col = 1
+
+with open(file) as f:
+    for line in f:
+
+        if "^" in line:
+            start_col += line.index("^")
+            start_found = True
+
+        if not start_found:
+            start_row += 1
+
+        row = "$" + line.strip() + "$"
+        board.append(list(row))
+
+
+board.append(pad)
+
+
+# I think you are supposed to use enums for this
+# remember: (row, col)
+UP = (-1, 0)
+DOWN = (1, 0)
+LEFT = (0, -1)
+RIGHT = (0, 1)
+
+moves = [UP, RIGHT, DOWN, LEFT]
+
+
+def move(pos, direction):
+
+    move = moves[direction]
+    next_pos = (pos[0] + move[0], pos[1] + move[1])
+
+    # if you hit an obsticle, turn 90 degrees instead of moving
+    if board[next_pos[0]][next_pos[1]] == "#":
+        direction = (direction + 1) % 4
+        return pos, direction
+
+    return next_pos, direction
+
+
+def patrol(start_row, start_col):
+
+    pos = (start_row, start_col)
+    direction = 0
+    found = {pos}
+
+    while board[pos[0]][pos[1]] != "$":
+
+        found.add(pos)
+        pos, direction = move(pos, direction)
+
+    print(len(found))
+
+
+patrol(start_row, start_col)
